c05777d4c2f160593d23b8fca45f60a864129565
Ualine055 <ualine055@gmail.com>
2025-11-06 00:36:47 +0200

    updated content in layout.tsx

--- next.js-demo/app/layout.tsx
+++ next.js-demo/app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css"
 import { Header } from "@/components/header"
 import { Footer } from "@/components/footer"
@@ -31,7 +31,7 @@ export default function RootLayout({
             <Footer />
           </div>
         </ThemeProvider>
-        <Analytics />
+        {/* <Analytics /> */}
       </body>
     </html>
   )
